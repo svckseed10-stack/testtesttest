4e1653fd4006c96f4048c6f92125043fdf4e1224
svckseed10-stack <svckseed10@gmail.com>
2025-11-26 01:24:20 +0700

    Add files via upload

---
+++ gold.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+    try {
+        const response = await fetch("https://api.treasury.id/api/v1/antigrvty/gold/rate", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: "{}"
+        });
+
+        const data = await response.json();
+        res.status(200).json(data);
+
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch data" });
+    }
+}
