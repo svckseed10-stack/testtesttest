c10f4e32cb594083107a9abb002e47121537c993
svckseed10-stack <svckseed10@gmail.com>
2025-11-26 01:39:53 +0700

    Update gold.js

--- api/gold.js
+++ api/gold.js
@@ -1,15 +1,21 @@
 export default async function handler(req, res) {
   try {
-    const url = "https://www.indogold.id/public/api/purities/5/products?lastPrice=1&orderBy=asc";
-    const apiRes = await fetch(url);
+    const url = "https://api.treasury.id/api/v1/antigrvty/gold/rate";
+
+    const apiRes = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
 
     if (!apiRes.ok) {
       return res.status(500).json({ error: "API gagal diambil" });
     }
 
-    const data = await apiRes.json();
+    const result = await apiRes.json();
 
-    return res.status(200).json(data);
+    return res.status(200).json(result.data);
   } catch (error) {
     return res.status(500).json({ error: "Server bermasalah" });
   }
